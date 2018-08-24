5193768f9913e0f06a744ae061ef909fddf2020c
Sang Le <sang@smartopia.io>
2018-08-25 00:32:37 +0700

    Before adding Express.js

---
+++ src/components/Contact.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-layout>
+      This is contact
+    </v-layout>
+</template>
+
+<script>
+    export default {
+        name: "Contact"
+    }
+</script>
+
+<style scoped>
+
+</style>
